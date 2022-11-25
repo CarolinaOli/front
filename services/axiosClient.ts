8a958889a81fae1b9615d3c6a2deef8cd45431a4
Carolina Olivares <38932158+CarolinaOli@users.noreply.github.com>
2022-11-24 23:34:30 -0300

    assa

--- services/axiosClient.ts
+++ services/axiosClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosClient = axios.create(
     {
-        baseURL: 'https://back65mas.herokuapp.com/api',
+        baseURL: 'https://back65mas.herokuapp.com:8000/api',
         // baseURL: 'https://backsistemascreening.herokuapp.com/api'
     }
 )
