df92b2534122a40b972603f8b019ac3ca7b05a1a
Carolina Olivares <38932158+CarolinaOli@users.noreply.github.com>
2022-11-30 23:31:14 -0300

    ssds

--- services/axiosClient.ts
+++ services/axiosClient.ts
@@ -5,7 +5,7 @@ axios.defaults.withCredentials = true
 const axiosClient = axios.create(
     {
         baseURL: 'https://back2-eight.vercel.app:8000/api',
-        timeout: 5000 * 30
+        timeout: 50000000000
         // baseURL: 'https://backsistemascreening.herokuapp.com/api'
     }
 )
