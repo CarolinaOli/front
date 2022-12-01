d6fd23bbc13add79d4a5add426dcdd76dfcf931f
Carolina Olivares <38932158+CarolinaOli@users.noreply.github.com>
2022-11-30 23:21:29 -0300

    assa

--- services/axiosClient.ts
+++ services/axiosClient.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
+axios.defaults.headers.post['Content-Type'] = 'aplication/json;charset=UTF-8'
+axios.defaults.withCredentials = true
 
 const axiosClient = axios.create(
     {
         baseURL: 'https://back2-eight.vercel.app:8000/api',
+        timeout: 5000
         // baseURL: 'https://backsistemascreening.herokuapp.com/api'
     }
 )
